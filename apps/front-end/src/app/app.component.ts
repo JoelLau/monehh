e24936793e778ab0bd5186478e61ecd97a74fcde
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-08-09 20:09:17 +0800

    removed nx-welcome component

--- apps/front-end/src/app/app.component.ts
+++ apps/front-end/src/app/app.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'front-end';
-
-  constructor(private http: HttpClient) {}
-  ngOnInit() {
-    console.log('done');
-  }
-}
+export class AppComponent {}
