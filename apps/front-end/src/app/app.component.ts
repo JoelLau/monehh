8838a995e46b441b3aad691845358d49bfecf8f7
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-08-09 19:06:01 +0800

    eslint + prettier integration

--- apps/front-end/src/app/app.component.ts
+++ apps/front-end/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'mn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    console.log('done');
+  }
 }
--- apps/front-end/src/app/app.module.ts
+++ apps/front-end/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    CommonModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
